e525e560676ee4ed8d328c01272fba3e1d9553b6
summer <levantung14112002@gmail.com>
2022-06-01 10:38:34 +0700

    first commit

---
+++ item.js
@@ -0,0 +1,65 @@
+function showThongTin(event)  {
+    var ShowThongTinItem = document.getElementById("ShowThongTinItem");
+    var heightValue = ShowThongTinItem.style.height;
+    if(heightValue !== "1090px") {
+        ShowThongTinItem.style.height = "1090px";
+    } else {
+        ShowThongTinItem.style.height = "auto";
+    }
+ }
+
+ function answer1(event) {
+     var answer = document.getElementById("TV-CauHoi1");
+     var heightValue1 = answer.style.height;
+     var backgroundColor =  answer.style.background;
+     if(heightValue1 !== "50px") {
+         answer.style.height = "50px";
+         answer.style.background = "white";
+     }
+     else {
+         answer.style.height = "auto";
+         answer.style.background = "#f8f9fa";
+     }
+ }
+
+ function answer2(event) {
+    var answer = document.getElementById("TV-CauHoi2");
+    var heightValue1 = answer.style.height;
+    var backgroundColor =  answer.style.background;
+    if(heightValue1 !== "50px") {
+        answer.style.height = "50px";
+        answer.style.background = "white";
+    }
+    else {
+        answer.style.height = "auto";
+        answer.style.background = "#f8f9fa";
+    }
+}
+
+function answer3(event) {
+    var answer = document.getElementById("TV-CauHoi3");
+    var heightValue1 = answer.style.height;
+    var backgroundColor =  answer.style.background;
+    if(heightValue1 !== "50px") {
+        answer.style.height = "50px";
+        answer.style.background = "white";
+    }
+    else {
+        answer.style.height = "auto";
+        answer.style.background = "#f8f9fa";
+    }
+}
+
+function answer4(event) {
+    var answer = document.getElementById("TV-CauHoi4");
+    var heightValue1 = answer.style.height;
+    var backgroundColor =  answer.style.background;
+    if(heightValue1 !== "50px") {
+        answer.style.height = "50px";
+        answer.style.background = "white";
+    }
+    else {
+        answer.style.height = "auto";
+        answer.style.background = "#f8f9fa";
+    }
+}
